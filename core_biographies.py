aeb43dcf6cfc84d3fbb1eba1028f9aeddd246465
Matt Monfils <mmonfils12@gmail.com>
2021-09-18 11:39:53 -0500

    Create core_biographies.py
    Adding python script showing where original data was obtained from Wikipedia

---
+++ core_biographies.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import requests
+
+# Writing a script to retrieve and locally save the data from the Wikipedia website
+
+url = "https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Biography/" \
+    "Core_biographies/list"
+
+r = requests.get(url)
+df_list = pd.read_html(r.text)
+df = df_list[0]
+df.to_csv('core_biographies.csv', index=False)
